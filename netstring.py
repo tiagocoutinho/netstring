67a8f1a60779ce916baccaef4a83461e5b53be18
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2022-03-25 19:50:29 +0100

    Bump version: 0.1.0 → 0.2.0

--- netstring.py
+++ netstring.py
@@ -5,7 +5,7 @@
 # Copyright (c) 2021 Tiago Coutinho
 # Distributed under the GPLv3 license. See LICENSE for more info.
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 END = b','
 END_ORD = ord(END)
